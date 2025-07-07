4718d27b1137861e67bdea1d25d1d31aee1582f4
eduardo-toste <duciochetti@gmail.com>
2025-07-06 21:18:45 -0300

    uploading initial project

---
+++ src/main/java/com/packmate/PackmateApplication.java
@@ -0,0 +1,13 @@
+package com.packmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packmate/PackmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
